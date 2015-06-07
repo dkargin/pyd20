180e80b87d8d1b7a39db14ebc8c5e25e0f389078
Philipp Jean-Jacques <philipp@hijacksoft.de>
2015-06-07 23:14:39 +0200

    restructured game-specific data to be located in a seperate module

---
+++ dnd/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from pyd20.character import Class, Skill, Race
+
+Class.load("dnd/classes.json")
+Skill.load("dnd/skills.json")
+Race.load("dnd/races.json")
+
+# Classes
+Barbarian = Class.with_name('barbarian')
+Bard = Class.with_name('bard')
+Druid = Class.with_name('druid')
+Cleric = Class.with_name('cleric')
+
+# Races
+Human = Race.with_name('human')
+Dwarf = Race.with_name('dwarf')
+Elf = Race.with_name('elf')
+Gnome = Race.with_name('gnome')
+HalfElf = Race.with_name('half-elf')
+HalfOrc = Race.with_name('half-orc')
+Halfling = Race.with_name('halfling')
