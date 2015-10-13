ae4007aefba64181eaf0c0cf29774eae3e6a81b4
Philipp Jean-Jacques <philipp@hijacksoft.de>
2015-10-13 10:57:35 +0200

    Added round resetting and combatant placing to Battle

--- battle.py
+++ battle.py
@@ -1,21 +1,55 @@
 import character
-import core
+
 
 class Battle:
 
-    def __init__(self):
+    """
+    Models a battle
+
+    :type _grid: grid.Grid
+    :type _combatants: Combatant[]
+    """
+
+    def __init__(self, grid):
+        """
+        Create an instance of a battle.
+
+        :param Grid grid: The grid the battle takes place on
+        """
+        self._grid = grid
+        self._combatants = []
         pass
+
+    def add_combatant(self, entity, x, y):
+        combatant = Combatant(entity)
+        position = self._grid.get_tile(x, y)
+        position.add_occupation(combatant)
+        self._combatants.append(combatant)
+
+    def next_round(self):
+        for combatant in self._combatants:
+            combatant.reset_round()
 
 
 class Combatant:
 
     """
-    :type _character: character.Character
+    :type _is_flat_footed: bool
+    :type _entity: character.Character
+    :type _action_points: int
     """
 
-    def __init__(self, character):
+    def __init__(self, entity):
+        """
+        :param character.Character entity:
+        """
+        self._entity = entity
         self._is_flat_footed = False
-        self._character = character
+        self._action_points = 3
+
+    def reset_round(self):
+        self._is_flat_footed = False
+        self._action_points = 3
 
     def initiative(self):
-        return self._character.initiative()
+        return self._entity.initiative()
